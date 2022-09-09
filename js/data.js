426b88122ce7db53aec80b5988d133ad2385af8c
davidv <davidvazquezgit@gmail.com>
2022-09-09 10:31:15 -0700

    add the board section with divs, add reset button and input text filed

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 const categoryOne = ["They say this member of the Mustelids goes POP", "I wonder if this greek hero heard a POP when his got torn" ]
 const categoryTwo = ["This triple crown winner had a hit movie in 2003 starring Tobey Maguire", "Name the actor: Speed, Point Break, The Devils Advocate, Bill and Ted's Execellent Adventure"]
 const categoryThree = ["If you say his name before you shoot a crumpled piece of paper, your odds increase of it going in", "Name the team: Troy, Emmitt, Michale, Jimmy"]
-const categoryFour = ["Name the group: UGod, Inspecta Deck, GZA, Raekwon the Chef",""]
+const categoryFour = ["Name the group: UGod, Inspecta Deck, GZA, Raekwon the Chef","This singer said 'Its ok to eat fish Cause they dont have any feelings'"]
 
-const answers = ['seabiscuit', 'achilles', 'weasel', 'keanureeves', 'wutang', 'wutangclan', 'kobe', 'cowboys', 'dallascowboys']
+const answers = ['seabiscuit', 'achilles', 'weasel', 'keanureeves', 'wutang', 'wutangclan', 'kobe', 'cowboys', 'dallascowboys', 'kurtcobain']
