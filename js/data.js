a9b3a2dd8c4fe67a0955316f5d526ec669dca5e5
davidv <davidvazquezgit@gmail.com>
2022-09-09 10:06:35 -0700

    initilize repo and start the data.js variables

---
+++ js/data.js
@@ -0,0 +1,6 @@
+const categoryOne = ["They say this member of the Mustelids goes POP", "I wonder if this greek hero heard a POP when his got torn" ]
+const categoryTwo = ["This triple crown winner had a hit movie in 2003 starring Tobey Maguire", "Name the actor: Speed, Point Break, The Devils Advocate, Bill and Ted's Execellent Adventure"]
+const categoryThree = ["If you say his name before you shoot a crumpled piece of paper, your odds increase of it going in", "Name the team: Troy, Emmitt, Michale, Jimmy"]
+const categoryFour = ["Name the group: UGod, Inspecta Deck, GZA, Raekwon the Chef",""]
+
+const answers = ['seabiscuit', 'achilles', 'weasel', 'keanureeves', 'wutang', 'wutangclan', 'kobe', 'cowboys', 'dallascowboys']
